cae3616935a25666bc2603eddb6df9506bcb46ae
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-20 05:30:35 +0200

    front/feat: ajout des fonctionalité de enregistrement

--- AngularAuthUI/src/app/components/signup/signup.component.ts
+++ AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { SignIn } from '../../model/signIn';
+import { AuthentificationService } from '../../service/authentification.service';
+import { FormsModule } from '@angular/forms';
+import { Message } from '../../model/message';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
@@ -11,14 +16,28 @@ export class SignupComponent implements OnInit{
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash"
-  constructor() {}
+  utilisateur: SignIn= {} as SignIn;
+  response: Message={message:'S'};
 
+  constructor(private api: AuthentificationService) {}
   ngOnInit(): void {}
 
-  
   hideShowPass(){
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onSignIn(){
+    this.api.signIn(this.utilisateur).subscribe(
+      (response: Message) => {
+        this.response = response;
+        console.log('Login successful', this.response);
+        alert(response.message);
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Login failed', error.message);
+      }
+    )
   }
 }
