d6b75b14e4d7fc0733610c21dda1f0d7018943b3
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-20 05:14:40 +0200

    front/feat: ajout des fonctionalités de conexion par l'api

--- AngularAuthUI/src/app/components/login/login.component.ts
+++ AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../../model/login';
+import { FormsModule } from '@angular/forms';
+import { AuthentificationService } from '../../service/authentification.service';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { LoginResponse } from '../../model/login-response';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   type: string = "password";
   isText: boolean = false;
-  eyeIcon: string = "fa-eye-slash"
-  constructor() {}
+  eyeIcon: string = "fa-eye-slash";
+  login: Login = { email: '', motdepasse: '' };
+  response: LoginResponse={} as LoginResponse;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthentificationService) {}
+
+  ngOnInit(): void {}
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.eyeIcon = this.isText ? "fa-eye" : "fa-eye-slash";
+    this.type = this.isText ? "text" : "password";
   }
 
-  hideShowPass(){
-    this.isText = !this.isText;
-    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
-    this.isText ? this.type = "text" : this.type = "password";
+  onLogin() {
+    this.authService.login(this.login).subscribe(
+      (response: LoginResponse) => {
+        this.response = response;
+        console.log('Login successful', this.response);
+        alert(response.message);
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Login failed', error.message);
+      }
+    );
   }
 }
