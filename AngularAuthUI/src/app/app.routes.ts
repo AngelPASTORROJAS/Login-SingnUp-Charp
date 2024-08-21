bf4cf516cbe3db7ce83cfd4bcdd4f84c359e4f61
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-21 12:03:53 +0200

    feat: ajout des import dynamique pour les composant ( lazy loading )

--- AngularAuthUI/src/app/app.routes.ts
+++ AngularAuthUI/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full'},
+    {
+        path: 'login',
+        loadComponent: () => import('./components/login/login.component').then((m) => m.LoginComponent)
+    },
+    {
+        path: 'signup',
+        loadComponent: () => import('./components/signup/signup.component').then((m) => m.SignupComponent)
+    },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
 ];
